a15e4c703b829b7c2b838fe222770e9ccad482ce
Al Johri <al.johri@gmail.com>
2014-06-03 14:40:53 -0500

    Added comments.

--- run.py
+++ run.py
@@ -31,16 +31,25 @@ POSITION_ELEMENTS = ['offsetParent', 'offsetLeft', 'offsetTop', 'scrollLeft', 's
 
 with ChromeDevTools(sys.argv[1]) as cdt:
 
+    # Get document object and find id of root node (usually starts with 1)
     doc = cdt.dom.get_document()
     root_id = doc['root']['nodeId']
+
+    # Get HTML of the document object
     dom = cdt.dom.get_outer_html(root_id)['outerHTML']
 
+    # Inject javascript to find clickable elements
     ret_clickable = cdt.runtime.evaluate(JS_FIND_CLICKABLE_ELEMENTS)
     remote_object = ret_clickable['objectId']
+
+    # If return type is an Array, loop through all properties of the return
+    # object and only return the array indicies. Leave off the javascript 
+    # methods and prototype.
     if ret_clickable['className'] == "Array":
         object_properties = cdt.runtime.get_properties(remote_object)
         elements = list(filter(lambda prop: prop['name'].isdigit(), object_properties))
 
+    # Print list of elements
     for el in elements:
         print(el)
 
