6448993152a5cb83916f458cef674e75483abfed
Al Johri <al.johri@gmail.com>
2014-06-03 20:18:54 -0500

    Updated run.py example.

--- run.py
+++ run.py
@@ -6,16 +6,17 @@ POSITION_ELEMENTS = ['offsetParent', 'offsetLeft', 'offsetTop', 'scrollLeft', 's
 
 with ChromeDevTools(sys.argv[1]) as cdt:
 
-    # dom = cdt.get_dom()
-    # els1 = cdt.get_clickable()
-    # els2 = cdt.get_scrollable()
+    dom = cdt.get_dom()
+    print(dom)
 
-    ret2 = cdt.type("asdfasdf")
-    ret1 = cdt.click(0, 0)
+    els1 = cdt.get_clickable()
+    els2 = cdt.get_scrollable()
+
+    print(els1)
+    print(els2)
+
+    ret1 = cdt.type("asdfasdf")
+    ret2 = cdt.click(0, 0)
     
-
-    # print(dom)
-    # print(els1)
-    # print(els2)
     print(ret1)
     print(ret2)
