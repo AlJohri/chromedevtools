f1e0414b03e24448e763fbd3aafdc7d4c5ab664f
Al Johri <al.johri@gmail.com>
2014-06-05 13:56:08 -0500

    Grab xpath of all elements.

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-import sys, six
+import sys, six, lxml.etree
 
 if six.PY2:
     import __builtin__
@@ -11,23 +11,28 @@ POSITION_ELEMENTS = ['offsetParent', 'offsetLeft', 'offsetTop', 'scrollLeft', 's
 with ChromeDevTools(sys.argv[1]) as cdt:
 
     dom = cdt.get_dom()
-    print(dom)
+    tree = lxml.etree.ElementTree(dom)
+    for e in dom.iter():
+        print tree.getpath(e)
 
-    els1 = cdt.get_clickable()
-    els2 = cdt.get_scrollable()
+    # els1 = cdt.get_clickable()
+    # els2 = cdt.get_scrollable()
 
-    print(els1)
-    print(els2)
+    # print(els1)
+    # print(els2)
 
-    ret1 = cdt.type("asdfasdf")
-    ret2 = cdt.click(0, 0)
+    # ret1 = cdt.type("asdfasdf")
+    # ret2 = cdt.click(0, 0)
     
-    print(ret1)
-    print(ret2)
+    # print(ret1)
+    # print(ret2)
 
-    color = {'r': 255, 'g': 0, 'b': 0}
-    ret3 = cdt.dom.highlight_rect(10,10,100,100,color)
+    # color = {'r': 255, 'g': 0, 'b': 0}
+    # ret3 = cdt.dom.highlight_rect(10,10,100,100,color)
 
-    input("Alt-Tab to your Chrome Browser and see the highlighted rectangle.")
+    # input(
+    #     "Alt-Tab to your Chrome Browser and see the highlighted rectangle." +
+    #     "Then, press Enter to terminate."
+    # )
 
-    print(ret3)
+    # print(ret3)
